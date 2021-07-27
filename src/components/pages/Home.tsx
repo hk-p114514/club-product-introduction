1e85b8e13b042a1b977b948d770aa2f61081280a
hk-p114514 <hukada.gogotea@gmail.com>
2021-07-28 06:39:08 +0900

    fetch api

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,19 +1,13 @@
 import { FC } from "react";
-<<<<<<< HEAD
 import { Helmet } from "react-helmet";
-=======
->>>>>>> 5090e4a45e038f26762ae314c98bb7992893b02f
 import "../../index.css";
 
 const Home: FC = () => {
   return (
     <>
-<<<<<<< HEAD
       <Helmet>
         <title>Home</title>
       </Helmet>
-=======
->>>>>>> 5090e4a45e038f26762ae314c98bb7992893b02f
       <h1 className="text-x1">This is Home component</h1>
     </>
   );
