4e1adfd74ed5eb04e79be735da84e0bf68ab730b
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-10 22:19:33 +0900

    eslintの環境構築を完了

--- hamako-festival-introduction/src/App.tsx
+++ hamako-festival-introduction/src/App.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react';
 
-const App: FC = () => (
-  <>
-    <div></div>
-  </>
-);
+// const App: FC = () => (
+// <>
+// <div></div>
+// </>
+// );
+
+const App: FC = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
 
 export default App;
