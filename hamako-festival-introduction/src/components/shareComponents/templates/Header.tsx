8e60f70733404ac2f53107d42fd6255d6f0843ab
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-11 17:50:25 +0900

    Header.tsxを作成

--- hamako-festival-introduction/src/components/shareComponents/templates/Header.tsx
+++ hamako-festival-introduction/src/components/shareComponents/templates/Header.tsx
@@ -1,10 +1,22 @@
 // ヘッダー部分
+import { Title } from '../organisms/Title';
 import { FC } from 'react';
+import { YearNumberLogo } from '../molecules/YearNumberLogo';
 
-const Header: FC = () => (
-  <>
-    <div></div>
-  </>
-);
+type Props = {
+  title: string;
+  year: number;
+};
+
+const Header: FC<Props> = (props: Props) => {
+  const { title, year } = props;
+
+  return (
+    <section>
+      <Title title={title} />
+      <YearNumberLogo year={year} />
+    </section>
+  );
+};
 
 export { Header };
