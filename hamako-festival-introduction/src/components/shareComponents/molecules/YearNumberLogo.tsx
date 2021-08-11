524691f9a65991248c834174fe9b73f609c83b6a
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-11 17:45:13 +0900

    YearNumberLogoを作成

--- hamako-festival-introduction/src/components/shareComponents/molecules/YearNumberLogo.tsx
+++ hamako-festival-introduction/src/components/shareComponents/molecules/YearNumberLogo.tsx
@@ -1,1 +1,14 @@
+import { FC } from 'react';
+
+type Props = {
+  year: number;
+};
+
 // 年号ロゴ
+const YearNumberLogo: FC<Props> = (props: Props) => {
+  const { year } = props;
+
+  return <p>{year}年浜工祭</p>;
+};
+
+export { YearNumberLogo };
