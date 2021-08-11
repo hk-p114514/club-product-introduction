61535c996595c9c724390a149a0262d23a0476c8
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-11 12:05:41 +0900

    slideshowにとりかかった

--- hamako-festival-introduction/src/components/singleExhibit/templates/SlideShow.tsx
+++ hamako-festival-introduction/src/components/singleExhibit/templates/SlideShow.tsx
@@ -1,11 +1,42 @@
 // 紹介文横の画像、動画のスライドショー
 
 import { FC } from 'react';
+import Slider from 'react-slick';
+import { imageInfo } from '../../../data/images';
+import { SlideShowImage } from '../organisms/SlideShowImage';
 
-const SlideShow: FC = () => {
+const settings = {
+  // 操作系
+  dots: true,
+  arrows: true,
+  // 自動再生
+  autoplay: true,
+  pauseOnHover: true,
+  // 繰り返し
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+type Props = {
+  studentNumber: number;
+};
+
+const SlideShow: FC<Props> = (props: Props) => {
+  const { studentNumber } = props;
+
   return (
     <>
-      <div></div>
+      <Slider {...settings}>
+        {imageInfo.map((info) => {
+          info.images.map((image) => {
+            <SlideShowImage image={image} />;
+          });
+        })}
+      </Slider>
     </>
   );
 };
+
+export { SlideShow };
