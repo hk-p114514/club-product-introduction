9de2ee4e9392c15754fa3a5051b06ab22c81a9de
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-12 19:13:10 +0900

    リンクの表示に成功

--- hamako-festival-introduction/src/components/ListOfExhibits/templates/Exhibits.tsx
+++ hamako-festival-introduction/src/components/ListOfExhibits/templates/Exhibits.tsx
@@ -1,10 +1,21 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { data } from '../../../data/data';
 
 // 展示数分の紹介リンク
 const Exhibits: FC = () => {
   return (
     <>
-      <ul>{}</ul>
+      <ul>
+        {data.map((personalData) => {
+          const { id, title } = personalData;
+          return (
+            <li>
+              <Link to={`no${id}`}>{title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
