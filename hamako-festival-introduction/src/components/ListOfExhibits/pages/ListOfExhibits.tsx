21ce40cbecf3c918ea41f847001a85a2efd14173
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-14 16:28:35 +0900

    ホームへ戻ったときにタイトルを変更

--- hamako-festival-introduction/src/components/ListOfExhibits/pages/ListOfExhibits.tsx
+++ hamako-festival-introduction/src/components/ListOfExhibits/pages/ListOfExhibits.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
+import { Helmet } from 'react-helmet';
 import { Title } from '../../shareComponents/organisms/Title';
 import { Exhibits } from '../templates/Exhibits';
 
 // 全ての展示の一覧
 const ListOfExhibits: FC = () => {
+  const year: number = 2021;
   return (
     <>
-      <Title title="2021浜工祭" />
+      <Helmet>
+        <title>{`${year}浜工祭`}</title>
+      </Helmet>
+      <Title title={`${year}浜工祭`} />
       <Exhibits />
     </>
   );
