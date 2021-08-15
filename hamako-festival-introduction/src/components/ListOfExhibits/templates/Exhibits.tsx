abdccac586d6fef42f7bb54179441946a5c1f1f8
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-16 06:27:03 +0900

    マウスホバー時のアニメーションを追加

--- hamako-festival-introduction/src/components/ListOfExhibits/templates/Exhibits.tsx
+++ hamako-festival-introduction/src/components/ListOfExhibits/templates/Exhibits.tsx
@@ -6,21 +6,26 @@ import { data } from '../../../data/data';
 
 const useStyle = makeStyles(() =>
   createStyles({
+    contents: {
+      width: '100%',
+    },
     container: {
       width: '95%',
       margin: '0 auto',
       marginTop: '1%',
     },
     simpleLink: {
-      textDecoration: 'none',
+      transition: '0.5s',
       opacity: '0.85',
-      transition: '1s',
+      textDecoration: 'none',
       '&:hover': {
         opacity: '1',
+        transform: 'scale(1.1, 1.1)',
       },
     },
     exhibitLinkButton: {
       padding: 0,
+      overflow: 'hidden',
     },
   })
 );
@@ -29,28 +34,32 @@ const useStyle = makeStyles(() =>
 const Exhibits: FC = () => {
   const classes = useStyle();
   return (
-    <div className={classes.container}>
-      <Grid container className="exhibits" spacing={3}>
-        {data.map((personalData, i) => {
-          const { id, title } = personalData;
-          return (
-            <Grid item sm={12} md={6} lg={4} key={i}>
-              <Button
-                className={classes.exhibitLinkButton}
-                style={{ padding: 0 }}
-              >
-                <Link to={`no${id}`} className={classes.simpleLink}>
-                  <Paper elevation={3} className="paper">
-                    <img src={data[id].images[0]} alt="" />
-                    <h2>{title}</h2>
-                  </Paper>
-                </Link>
-              </Button>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </div>
+    <>
+      <section className={classes.contents}>
+        <div className={classes.container}>
+          <Grid container className="exhibits" spacing={3}>
+            {data.map((personalData, i) => {
+              const { id, title } = personalData;
+              return (
+                <Grid item sm={12} md={6} lg={4} key={i}>
+                  <Button
+                    className={classes.exhibitLinkButton}
+                    style={{ padding: 0 }}
+                  >
+                    <Link to={`no${id}`} className={classes.simpleLink}>
+                      <Paper elevation={3} className="paper">
+                        <img src={data[id].images[0]} alt="" />
+                        <h2>{title}</h2>
+                      </Paper>
+                    </Link>
+                  </Button>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </div>
+      </section>
+    </>
   );
 };
 
