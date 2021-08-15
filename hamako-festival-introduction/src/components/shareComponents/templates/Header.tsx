e59b09c1b7da9410df796dd6b078cd9b22780bdc
hk-p114514 <hukada.gogotea@gmail.com>
2021-08-15 17:30:47 +0900

    material-uiからヘッダをとってきた

--- hamako-festival-introduction/src/components/shareComponents/templates/Header.tsx
+++ hamako-festival-introduction/src/components/shareComponents/templates/Header.tsx
@@ -1,21 +1,42 @@
 // ヘッダー部分
+import { AppBar, makeStyles, Theme, useTheme } from '@material-ui/core';
 import { Title } from '../organisms/Title';
 import { FC } from 'react';
-import { YearNumberLogo } from '../molecules/YearNumberLogo';
+import { PageController } from './PageController';
+import './header.css';
 
 type Props = {
-  title: string;
   year: number;
 };
 
+const useStyle = (theme: Theme) => {
+  return makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+    flex: {
+      flex: 1,
+    },
+    toolbarMargin: theme.mixins.toolbar,
+  });
+};
+
 const Header: FC<Props> = (props: Props) => {
-  const { title, year } = props;
+  const { year } = props;
+  const title: string = `${year}浜工祭`;
+  const theme = useTheme();
+  const styles = useStyle(theme)();
 
   return (
-    <section>
-      <Title title={title} />
-      <YearNumberLogo year={year} />
-    </section>
+    <div className={styles.root}>
+      <AppBar position="fixed" className="appBar">
+        <div className="appBar-contents">
+          <PageController />
+          <Title title={title} />
+        </div>
+      </AppBar>
+      <div className={styles.toolbarMargin} />
+    </div>
   );
 };
 
